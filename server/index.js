724be3e3241923afc34ff5d6df897acd3222ef70
TataDr <druzhkova.tn@gmail.com>
2022-11-09 15:26:06 +0300

    edit index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 // const sequelize = require("./db");
-const models = require("./models/models");
+// const models = require("./models/models");
 const cors = require("cors");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
